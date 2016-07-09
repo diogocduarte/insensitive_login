9be65b1071fe65cd817393a1759e793b87b27e1e
Diogo Duarte <diogocarvalhoduarte@gmail.com>
2016-07-09 12:19:03 +0100

    init commit

---
+++ login_casei/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import openerp
+import logging
+from openerp.osv import  osv
+from openerp import SUPERUSER_ID
+
+_logger = logging.getLogger(__name__)
+
+
+class users(osv.osv):
+    _inherit = "res.users"
+    
+    def _login(self, db, login, password):
+        if not password:
+            return False
+        user_id = False
+        try:
+            with self.pool.cursor() as cr:
+                res = self.search(cr, SUPERUSER_ID, [('login', 'ilike', login)])
+                if res:
+                    user_id = res[0]
+                    self.check_credentials(cr, user_id, password)
+                    self._update_last_login(cr, user_id)
+        except openerp.exceptions.AccessDenied:
+            _logger.info("Login failed for db:%s login:%s", db, login)
+            user_id = False
+        return user_id
---
+++ login_casei/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Insensitive Login',
+    'version': '1.1',
+    'category': 'Web',
+    'summary': 'Implements case insensitive login',
+    'description': """
+Implements case insensitive login
+=================================
+    """,
+    'website': 'https://diogocduarte.github.io',
+    'depends': [
+        'web',
+    ],
+    'data': [
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'qweb': [],
+}
