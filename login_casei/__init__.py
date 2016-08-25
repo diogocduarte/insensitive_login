7592c8c959fdfe225ab7746398af6ae6d01b0719
Diogo Duarte <diogocarvalhoduarte@gmail.com>
2016-08-25 22:08:21 +0100

    [FIX] operator wrong

--- login_casei/__init__.py
+++ login_casei/__init__.py
@@ -9,14 +9,14 @@ _logger = logging.getLogger(__name__)
 
 class users(osv.osv):
     _inherit = "res.users"
-    
+
     def _login(self, db, login, password):
         if not password:
             return False
         user_id = False
         try:
             with self.pool.cursor() as cr:
-                res = self.search(cr, SUPERUSER_ID, [('login', 'ilike', login)])
+                res = self.search(cr, SUPERUSER_ID, [('login', '=ilike', login)])
                 if res:
                     user_id = res[0]
                     self.check_credentials(cr, user_id, password)
